0c215aac368053eb794c2ff6db956e5b1b7f4fe8
emmanuel <sunartio.emmanuel@gmail.com>
2022-09-10 10:56:24 +0930

    call function in parent activity which returns string

--- app/src/main/java/com/example/senddatafromactivitytofragment/FirstFragment.kt
+++ app/src/main/java/com/example/senddatafromactivitytofragment/FirstFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class FirstFragment : Fragment() {
+
+    lateinit var text: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val activity = MainActivity()
+        text = activity.getMyData()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.TextView).text = text
     }
 }
--- app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
+++ app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.senddatafromactivitytofragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val myString = "hello"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mEditText = findViewById<EditText>(R.id.editText)
-        val mButton = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+        val btn = findViewById<Button>(R.id.button)
 
+        // Declaring fragment manager from making data
+        // transactions using the custom fragment
+        val mFragmentManager = supportFragmentManager
+        val mFragmentTransaction = mFragmentManager.beginTransaction()
+        val mFragment = FirstFragment()
+
+        btn.setOnClickListener() {
+            val myString = editText.text.toString()
+            mFragmentTransaction.add(R.id.frameLayout, mFragment).commit()
+        }
+    }
+
+    fun getMyData(): String {
+        return myString
     }
 }
