eabeba4bfd442db07a285ea6f591018c5b029efc
emmanuel <sunartio.emmanuel@gmail.com>
2022-10-05 10:34:40 +1030

    completed: pass data using bundle

--- app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
+++ app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.senddatafromactivitytofragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -7,20 +8,30 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
-    private lateinit var sendText: EditText
+    lateinit var etId: EditText
+    lateinit var etName: EditText
+    lateinit var etRoll: EditText
+    lateinit var btnSend: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
-        sendText = findViewById(R.id.editText)
+        etId = findViewById(R.id.etId)
+        etName = findViewById(R.id.etName)
+        etRoll = findViewById(R.id.etRoll)
+        btnSend = findViewById(R.id.btnSend)
 
-        button.setOnClickListener() {
-            val fragment = DisplayTextFragment.newInstance(sendText.text.toString())
+        btnSend.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("id", etId.text.toString())
+            bundle.putString("name", etName.text.toString())
+            bundle.putString("roll", etRoll.text.toString())
 
-            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
         }
     }
+
 }
