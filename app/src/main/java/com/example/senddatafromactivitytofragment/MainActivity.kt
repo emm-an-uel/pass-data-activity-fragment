cae035253dc135f4a991a1981c7df1325aced77f
emmanuel <sunartio.emmanuel@gmail.com>
2022-10-06 10:47:29 +1030

    revert to using bundle, activity to activity

--- app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
+++ app/src/main/java/com/example/senddatafromactivitytofragment/MainActivity.kt
@@ -2,52 +2,35 @@ package com.example.senddatafromactivitytofragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var etId: EditText
     lateinit var etName: EditText
-    lateinit var etAge: EditText
-    lateinit var etGender: EditText
-    lateinit var btnSave: Button
+    lateinit var etRoll: EditText
     lateinit var btnSend: Button
-    lateinit var listPerson: ArrayList<Person>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        etId = findViewById(R.id.etId)
         etName = findViewById(R.id.etName)
-        etAge = findViewById(R.id.etAge)
-        etGender = findViewById(R.id.etGender)
-        btnSave = findViewById(R.id.btnSave)
+        etRoll = findViewById(R.id.etRoll)
         btnSend = findViewById(R.id.btnSend)
-        listPerson = ArrayList()
-
-        btnSave.setOnClickListener {
-            val name = etName.text.toString()
-            val age = etAge.text.toString()
-            val gender = etGender.text.toString()
-
-            val person = Person(name, age, gender)
-            listPerson.add(person)
-
-            // val listSize = listPerson.size
-            // Log.i("listSize", listSize.toString())
-        }
 
         btnSend.setOnClickListener {
-
             val bundle = Bundle()
-            bundle.putParcelableArrayList("listPerson", listPerson)
-            val displayDataFragment = DisplayDataFragment()
-            displayDataFragment.arguments = bundle
+            bundle.putString("id", etId.text.toString())
+            bundle.putString("name", etName.text.toString())
+            bundle.putString("roll", etRoll.text.toString())
 
-            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, displayDataFragment)
-                .commit()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
         }
     }
 
